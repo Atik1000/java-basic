a7d941f72cdd59bd178b36d31228b2595928f98c
Atik1000 <foysalkazi45@gmail.com>
2022-08-26 23:42:43 +0600

    basic java concept

---
+++ sampleinput.java
@@ -0,0 +1,13 @@
+
+import java.util.*;
+
+public class DisplayNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double n = sc.nextDouble();
+        double m = sc.nextDouble();
+        System.out.println("Fist number:"+n);
+        System.out.println("Seconds number: "+m);
+     
+    }
+}
